996f21aeb7e9bf274b7c62756f2a273b18073699
JnChan823 <1786171559@qq.com>
2023-09-04 10:25:29 +0800

    订单详情--订单状态渲染

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,4 +1,4 @@
-import type { OrderPreResult } from '@/types/order'
+import type { OrderCreateParams, OrderPreResult, OrderResult } from '@/types/order'
 import { http } from '@/utils/http'
 
 /**
@@ -28,5 +28,32 @@ export const getMemberOrderPreNowAPI = (data: {
     method: 'GET',
     url: '/member/order/pre/now',
     data,
+  })
+}
+
+/**
+ * 提交订单
+ * POST
+ * /member/order
+ * @param data 请求参数
+ */
+export const postMemberOrderAPI = (data: OrderCreateParams) => {
+  return http<{ id: string }>({
+    method: 'POST',
+    url: '/member/order',
+    data,
+  })
+}
+
+/**
+ * 获取订单详情
+ * GET
+ * /member/order/{id}
+ * @param id 订单id
+ */
+export const getMemberOrderByIdAPI = (id: string) => {
+  return http<OrderResult>({
+    method: 'GET',
+    url: `/member/order/${id}`,
   })
 }
